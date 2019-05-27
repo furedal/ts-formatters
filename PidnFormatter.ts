02b299c84711b13738340eef4eba55fe5a6025af
furedal <cfuredal@gmail.com>
2019-05-27 17:46:49 +0200

    support for 10 digits

--- PidnFormatter.ts
+++ PidnFormatter.ts
@@ -8,9 +8,27 @@ export const PidnFormatter: (string, boolean?) => string = (
   if (!value) {
     return ''
   }
-  const formatted = value.replace(/\D/g, '')
+  let formatted = value.replace(/\D/g, '')
+  formatted = validatedYear(formatted)
+
+  if (value.endsWith('-') && formatted.length === 8) {
+    return `${formatted}-`
+  }
+
   if (formatted.length > 8) {
     return formatted.slice(0, 8) + '-' + formatted.slice(8)
   }
   return formatted
+}
+
+const validatedYear = (value: string) => {
+  if (value.length >= 2) {
+    const prefix = parseInt(value.slice(0, 2), 10)
+    if (prefix != 19 && prefix != 20) {
+      const yearDiff = new Date().getFullYear() % 1000
+      const prepend = prefix >= yearDiff ? '19' : '20'
+      return prepend + value
+    }
+  }
+  return value
 }
